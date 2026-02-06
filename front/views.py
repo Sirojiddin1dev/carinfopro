d49f8c3842a4c8d7fd527f20077ace5c5faa8e8c
user <maxalone155@gmail.com>
2026-02-06 14:40:36 +0500

    sirojiddin

--- front/views.py
+++ front/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from main.models import User
 
 
 def index(request):
@@ -6,4 +7,14 @@ def index(request):
 
 
 def user_chat(request, user_id):
-    return render(request, 'front/user.html', {'user_id': str(user_id)})
+    user = get_object_or_404(User, id=user_id, is_active=True)
+    if not user.is_profile_public:
+        return render(request, 'front/user.html', {
+            'user_id': str(user_id),
+            'is_public': False
+        })
+    return render(request, 'front/user.html', {
+        'user_id': str(user_id),
+        'is_public': True,
+        'user': user
+    })
